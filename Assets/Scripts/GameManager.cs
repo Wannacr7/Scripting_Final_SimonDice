6c4aa07f5dccfbb84c9694a4270fac6844c3b330
Wannacr7 <88002544+Wannacr7@users.noreply.github.com>
2021-11-07 15:41:48 -0500

    Maquina funcionando
    Recibe la cantidad de colores y la cantidad de veces que se repite la secuencia

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,116 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public enum EColors
 {
-    Red,
-    Yellow,
-    blue,
-    Green,
-    Purple
+    Red=0,
+    Yellow=1,
+    Blue=2,
+    Green=3,
+    Purple=4
 }
 public class GameManager : MonoBehaviour
 {
+    private EColors _typecolor;
+    //Colores
+    [SerializeField] private Renderer red, yellow, blue, green, purple;
+    [SerializeField] AnimationCurve colorCurve;
+    
+
+    public Action<bool> On_Enable_Machine;
+    private bool startMachine = false;
+    private bool startGame = false;
+    private int[] machineArray;
+
+
+    private float timeChangeColor = 1f;
+    private float timer = 0;
+    private float lerpRatio;
+
+    [SerializeField]private int level = 1;
+    [SerializeField]private int levelCounter=0;
+
+    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        On_Enable_Machine += StartEvent;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (startMachine == true)
+        {
+            SetMachineColor();
+            
+        }
+
+
+    }
+
+
+    private void SetMachineColor()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeChangeColor)
+        {
+            timer = 0;
+            if (levelCounter < level) levelCounter++;
+            else
+            {
+                startMachine = false;
+                levelCounter = 0;
+            } 
+           
+
+        }
+        lerpRatio = timer / timeChangeColor;
+        _typecolor = (EColors)machineArray[levelCounter];
+        switch (_typecolor)
+        {
+            case EColors.Red:
+                red.material.color = Color.LerpUnclamped(new Color(0.5f, 0.2f, 0.2f), new Color(1f, 0f, 0f), colorCurve.Evaluate(lerpRatio));
+                break;
+            case EColors.Yellow:
+                yellow.material.color = Color.LerpUnclamped(new Color(0.5f, 0.2f, 0.2f), new Color(1f, 0f, 0f), colorCurve.Evaluate(lerpRatio));
+                break;
+            case EColors.Blue:
+                blue.material.color = Color.LerpUnclamped(new Color(0.5f, 0.2f, 0.2f), new Color(1f, 0f, 0f), colorCurve.Evaluate(lerpRatio));
+                break;
+            case EColors.Green:
+                green.material.color = Color.LerpUnclamped(new Color(0.5f, 0.2f, 0.2f), new Color(1f, 0f, 0f), colorCurve.Evaluate(lerpRatio));
+                break;
+            case EColors.Purple:
+                purple.material.color = Color.LerpUnclamped(new Color(0.5f, 0.2f, 0.2f), new Color(1f, 0f, 0f), colorCurve.Evaluate(lerpRatio));
+                break;
+            default:
+                break;
+        }
+
+        
         
     }
+
+    private void  StartEvent(bool _event)
+    {
+        machineArray= SimonSayMachine.instance.GenerateArray(level + 1, 2);
+        startMachine = _event;
+        
+    }
+    public void testevent()
+    {
+        if (On_Enable_Machine != null)
+        {
+            On_Enable_Machine(true);
+        }
+        startGame = true;
+    }
+
 }
