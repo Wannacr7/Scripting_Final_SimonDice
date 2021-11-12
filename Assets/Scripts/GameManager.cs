5738528130aed78f5beed2875e86536dd72953c9
Ravenoid01 <juanda.rave147@gmail.com>
2021-11-12 18:04:31 -0500

    Pausa y UiController

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-
 public enum EColors
 {
     Red=0,
@@ -18,20 +17,11 @@ public class GameManager : MonoBehaviour
     //Colores
     [SerializeField] private Renderer red, yellow, blue, green, purple;
     [SerializeField] AnimationCurve colorCurve;
-    [SerializeField] GameObject TitleScreen;
-    [SerializeField] GameObject DifficultyScreen;
-    [SerializeField] GameObject StartButtom;
-    [SerializeField] GameObject Colores;
-    [SerializeField] GameObject CoverLayer1;
-    [SerializeField] GameObject CoverLayer2;
-    [SerializeField] AudioSource menuMusic;
-
 
     public Action<bool> On_Enable_Machine;
     private bool startMachine = false;
     private bool startGame = false;
     private int[] machineArray;
-
 
     private float timeChangeColor = 1f;
     private float timer = 0;
@@ -39,18 +29,11 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]private int level = 1;
     [SerializeField]private int levelCounter=0;
-    [SerializeField]private int difficulty = 0;
+    [SerializeField]public int difficulty = 0;
 
-
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
         On_Enable_Machine += StartEvent;
-
-        menuMusic.Play();
 
         red.material.color = new Color(0.5f, 0.1f, 0.1f);
         yellow.material.color= new Color(0.5f, 0.5f, 0.1f);
@@ -67,10 +50,7 @@ public class GameManager : MonoBehaviour
             SetMachineColor();
             
         }
-
-
     }
-
 
     private void SetMachineColor()
     {
@@ -109,9 +89,6 @@ public class GameManager : MonoBehaviour
             default:
                 break;
         }
-
-        
-        
     }
 
     private void  StartEvent(bool _event)
@@ -120,41 +97,6 @@ public class GameManager : MonoBehaviour
         startMachine = _event;
         
     }
-    public void testevent()
-    {
-        if (On_Enable_Machine != null)
-        {
-            On_Enable_Machine(true);
-        }
-        StartButtom.SetActive(false);
-        startGame = true;
-    }
-
-    public void PressPlay()
-    {
-        TitleScreen.SetActive(false);
-        DifficultyScreen.SetActive(true);
-    }
-    public void ChooseDifficulty()
-    {
-        DifficultyScreen.SetActive(false);
-        Colores.SetActive(true);
-        StartButtom.SetActive(true);
-    }
-    public void HardMode()
-    {
-        difficulty = 5;
-    }
-    public void MediumMode()
-    {
-        difficulty = 4;
-        CoverLayer2.SetActive(true);
-    }
-    public void EasyMode()
-    {
-        difficulty = 2;
-        CoverLayer1.SetActive(true);
-        
-    }
+    
 
 }
---
+++ Assets/Scripts/IUController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.IO;
+
+public class IUController : MonoBehaviour
+{
+    [SerializeField] private GameObject Colores;
+    [SerializeField] private GameObject CoverLayer1;
+    [SerializeField] private GameObject CoverLayer2;
+
+    [SerializeField] private GameManager gameManager;
+
+    [SerializeField] private GameObject StartButtom;
+    [SerializeField] private GameObject pauseButtom;
+
+    [SerializeField] private GameObject TitleScreen;
+    [SerializeField] private GameObject DifficultyScreen;
+    [SerializeField] private GameObject PauseScreen;
+
+    [SerializeField] AudioSource menuMusic;
+
+
+    private bool IsPaused = false;
+
+    private void Start()
+    {
+        menuMusic.Play();
+    }
+
+    public void testevent()
+    {
+        if (gameManager.On_Enable_Machine != null)
+        {
+            gameManager.On_Enable_Machine(true);
+        }
+        StartButtom.SetActive(false);
+        pauseButtom.SetActive(true);
+    }
+
+    public void PressPlay()
+    {
+        TitleScreen.SetActive(false);
+        DifficultyScreen.SetActive(true);
+    }
+    public void ChooseDifficulty()
+    {
+        DifficultyScreen.SetActive(false);
+        Colores.SetActive(true);
+        StartButtom.SetActive(true);
+    }
+    public void HardMode()
+    {
+        gameManager.difficulty = 5;
+    }
+    public void MediumMode()
+    {
+        gameManager.difficulty = 4;
+        CoverLayer2.SetActive(true);
+    }
+    public void EasyMode()
+    {
+        gameManager.difficulty = 2;
+        CoverLayer1.SetActive(true);
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void PauseButton()
+    {
+        if (IsPaused)
+        {
+            Time.timeScale = 1;
+            IsPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            IsPaused = true;
+        }
+    }
+
+}
