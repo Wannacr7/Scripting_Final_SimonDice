3682ad87579a96b5094c49e96af11b9bb798388e
DevUnity.IES <sebastian.coronado@iesonline.com.co>
2023-09-06 16:38:37 -0500

    code structured

---
+++ Assets/Scripts/Player/SimonSayPlayer.cs
@@ -0,0 +1,149 @@
+
+using Core;
+using UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Player
+{
+    public class SimonSayPlayer : MonoBehaviour
+    {
+        //timer de los colores
+        private float timeChangeColor = 1f;
+        private float timer = 0;
+        private float lerpRatio;
+
+        //Timer para el jugador
+        private float timeToCompleteSecuence = 1f;
+        private float counter = 0;
+
+        //Verifica secuencia
+        [SerializeField] private GameManager main;
+        [SerializeField] private int sequenceLenght;
+        private bool verifyNumber = false;
+
+
+        private EColors colorON;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            sequenceLenght = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (main.StartPlayer)
+            {
+                GetPlayerNum();
+                if (!(colorON is EColors.Null))
+                {
+                    AnimatorColors();
+                }
+                if (verifyNumber)
+                {
+                    CanContinue();
+                }
+
+            }
+
+
+        }
+
+        private void GetPlayerNum()
+        {
+            if (Input.GetKeyDown((KeyCode.Q)))
+            {
+                colorON = EColors.Red;
+                timer = 0;
+                verifyNumber = true;
+
+            }
+            if (Input.GetKeyDown((KeyCode.W)))
+            {
+                colorON = EColors.Yellow;
+                timer = 0;
+                verifyNumber = true;
+            }
+            if (Input.GetKeyDown((KeyCode.A)))
+            {
+                colorON = EColors.Blue;
+                timer = 0;
+                verifyNumber = true;
+            }
+            if (Input.GetKeyDown((KeyCode.S)))
+            {
+                colorON = EColors.Green;
+                timer = 0;
+                verifyNumber = true;
+            }
+            if (Input.GetKeyDown((KeyCode.D)))
+            {
+                colorON = EColors.Purple;
+                timer = 0;
+                verifyNumber = true;
+            }
+
+        }
+        private void AnimatorColors()
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeChangeColor)
+            {
+                timer = 0;
+                colorON = EColors.Null;
+            }
+            lerpRatio = timer / timeChangeColor;
+
+            switch (colorON)
+            {
+                case EColors.Red:
+                    ColorsGestor.instance.ChangeColor(colorON, lerpRatio);
+                    break;
+                case EColors.Yellow:
+                    ColorsGestor.instance.ChangeColor(colorON, lerpRatio);
+                    break;
+                case EColors.Blue:
+                    ColorsGestor.instance.ChangeColor(colorON, lerpRatio);
+                    break;
+                case EColors.Green:
+                    ColorsGestor.instance.ChangeColor(colorON, lerpRatio);
+                    break;
+                case EColors.Purple:
+                    ColorsGestor.instance.ChangeColor(colorON, lerpRatio);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void CanContinue()
+        {
+            int typeColor = (int)colorON;
+
+            Debug.Log((int)colorON + "  " + main.MachineArray[sequenceLenght]);
+            if (typeColor == main.MachineArray[sequenceLenght])
+            {
+                sequenceLenght++;
+                verifyNumber = false;
+                if (sequenceLenght == main.MachineArray.Length)
+                {
+                    main.Level++;
+                    Invoke("CallMachine", 2);
+                }
+            }
+            else
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+
+        private void CallMachine()
+        {
+            sequenceLenght = 0;
+            main.On_Enable_Machine?.Invoke(true);
+        }
+    }
+}
+
---
+++ Assets/Scripts/UI/IUController.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.IO;
+using TMPro;
+using Core;
+
+namespace UI
+{
+    public class IUController : MonoBehaviour
+    {
+        [SerializeField] private GameObject Colores;
+        [SerializeField] private GameObject CoverLayer1;
+        [SerializeField] private GameObject CoverLayer2;
+
+        [SerializeField] private GameManager gameManager;
+
+        [SerializeField] private GameObject StartButtom;
+        [SerializeField] private GameObject pauseButtom;
+        [SerializeField] public TMP_Text[] stateOfGame;
+        [SerializeField] public TMP_Text score;
+
+        [SerializeField] private GameObject TitleScreen;
+        [SerializeField] private GameObject DifficultyScreen;
+        [SerializeField] private GameObject PauseScreen;
+
+        [SerializeField] AudioSource menuMusic;
+
+        [SerializeField] GameObject backgroundNormal;
+        [SerializeField] GameObject backgroundMedium;
+        [SerializeField] GameObject backgroundHard;
+
+        [SerializeField] GameManager state;
+
+        private bool IsPaused = false;
+
+        private void Start()
+        {
+            menuMusic.Play();
+
+
+        }
+        private void Update()
+        {
+            ChangeState();
+        }
+        private void ChangeState()
+        {
+            if (state.StartMachine)
+            {
+
+                stateOfGame[0].text = "Maquina";
+                stateOfGame[1].text = "Maquina";
+                score.text = "0" + (state.MachineArray.Length - 1).ToString();
+            }
+            else
+            {
+                stateOfGame[0].text = "Jugador";
+                stateOfGame[1].text = "Jugador";
+
+            }
+        }
+
+        public void testevent()
+        {
+            if (gameManager.On_Enable_Machine != null)
+            {
+                gameManager.On_Enable_Machine(true);
+            }
+            StartButtom.SetActive(false);
+            stateOfGame[0].gameObject.SetActive(true);
+            score.gameObject.SetActive(true);
+            pauseButtom.SetActive(true);
+        }
+
+        public void PressPlay()
+        {
+            TitleScreen.SetActive(false);
+            DifficultyScreen.SetActive(true);
+        }
+        public void ChooseDifficulty()
+        {
+            DifficultyScreen.SetActive(false);
+            Colores.SetActive(true);
+            StartButtom.SetActive(true);
+        }
+        public void HardMode()
+        {
+            gameManager.difficulty = 5;
+            backgroundNormal.SetActive(false);
+            backgroundHard.SetActive(true);
+
+        }
+        public void MediumMode()
+        {
+            gameManager.difficulty = 4;
+            CoverLayer2.SetActive(true);
+            backgroundMedium.SetActive(true);
+            backgroundNormal.SetActive(false);
+        }
+        public void EasyMode()
+        {
+            gameManager.difficulty = 2;
+            CoverLayer1.SetActive(true);
+
+        }
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+        public void PauseButton()
+        {
+            if (IsPaused)
+            {
+                Time.timeScale = 1;
+                IsPaused = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                IsPaused = true;
+            }
+        }
+
+    }
+
+}
